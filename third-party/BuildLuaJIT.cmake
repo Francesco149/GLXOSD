7d4c0afa46a3cbac5f7b263a6c810b44ea0576a7
Franc[e]sco <lolisamurai@tfwno.gf>
2020-12-08 19:07:43 +0100

    update luaJIT to 2.1.0-beta3

--- third-party/BuildLuaJIT.cmake
+++ third-party/BuildLuaJIT.cmake
@@ -6,19 +6,19 @@ set(INSTALL_COMMAND
 		CFLAGS=-fPIC
 		BUILDMODE=static
 		Q=
-		PREFIX=${CMAKE_CURRENT_BINARY_DIR}/luajit-2.0.4
+		PREFIX=${CMAKE_CURRENT_BINARY_DIR}/luajit-2.1.0-beta3
 		install)
 
 ExternalProject_Add(project_luajit
-	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/luajit-2.0.4
-	URL ${PROJECT_SOURCE_DIR}/third-party/LuaJIT-2.0.4.tar.gz
+	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/luajit-2.1.0-beta3
+	URL ${PROJECT_SOURCE_DIR}/third-party/LuaJIT-2.1.0-beta3.tar.gz
 	CONFIGURE_COMMAND ""
 	BUILD_IN_SOURCE 1
 	BUILD_COMMAND ""
 	INSTALL_COMMAND "${INSTALL_COMMAND}")
 
 ExternalProject_Get_Property(project_luajit install_dir)
-include_directories(${install_dir}/include/luajit-2.0)
+include_directories(${install_dir}/include/luajit-2.1)
 
 add_library(luajit STATIC IMPORTED)
 set_property(TARGET luajit PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libluajit-5.1.a)
